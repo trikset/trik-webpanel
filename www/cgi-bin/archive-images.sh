afca269da15805fa147597641a61f993bab968bd
Andrei Khodko <andrei.khodko@gmail.com>
2020-01-26 15:55:13 +0300

    Fix license

--- www/cgi-bin/archive-images.sh
+++ www/cgi-bin/archive-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Copyright 2020 Andrei Khodko
+#Copyright 2020 Andrei Khodko and Cybertech Labs Co Ltd
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
