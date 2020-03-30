a74d44f6120132a68605a5a732e7e51aef54ebb5
Andrei <andrei.khodko@gmail.com>
2020-03-30 21:06:22 +0300

    Rebase merge
    asd

--- www/cgi-bin/set-videoport.sh
+++ www/cgi-bin/set-videoport.sh
@@ -13,18 +13,20 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License
 
-system_config=/home/root/trik/system-config.xml
+system_config=system-config.xml
 current_params=current-params
 
 read -r params
 
 ./notifyThenKill.sh "$(basename -- "$0")" $$ "$params"
 
-sed -ie "1s/$/ ${params}/" "$current_params"
+sed -i "4c${params}" "$current_params"
 
-sed -i "/<photo class=\"camera\"/c <photo class="camera" type=\"v4l2\" src=\"/dev/video${params}\" />" $system_config
-sed -i "/<camera type=/c <camera type=\"v4l2\" src=\"/dev/video${params}\" />" $system_config
+sed -i "/<photo class=\"camera/c <photo class=\"camera\" type=\"v4l2\" src=\"/dev/video${params}\" />" "$system_config"
+sed -i "/<camera type=/c <camera type=\"v4l2\" src=\"/dev/video${params}\" />" "$system_config"
+
+cp "$system_config" "$(realpath /home/root/trik/system-config.xml)"
 
 echo "HTTP/1.1 201 Modified"
-reboot
 
+killall trikGui
--- www/js/configurator.js
+++ www/js/configurator.js
@@ -47,10 +47,10 @@ const app = new Vue({
         e2: "encoder95",
         e3: "encoder95",
         e4: "encoder95",
-        m1: "jga25-371",
-        m2: "jga25-371",
-        m3: "jga25-371",
-        m4: "jga25-371",
+        m1: "motor100",
+        m2: "motor100",
+        m3: "motor100",
+        m4: "motor100",
         video1: "lineSensor",
         video2: "photo",
         getPhotoPort: "0",
@@ -135,7 +135,7 @@ const app = new Vue({
         this.m4 = ports[26];
         this.video1 = ports[27];
         this.video2 = ports[28];
-        this.getPhotoPort = ports[29];
+        this.getPhotoPort = text[3][0];
         this.gyroscope = ag[3];
         this.accelerometer = ag[0];
         this.gyroFreq = ag[4];
@@ -207,10 +207,10 @@ const app = new Vue({
             this.e2 = "encoder95";
             this.e3 = "encoder95";
             this.e4 = "encoder95";
-            this.m1 = "jga25-371";
-            this.m2 = "jga25-371";
-            this.m3 = "jga25-371";
-            this.m4 = "jga25-371";
+            this.m1 = "motor100";
+            this.m2 = "motor100";
+            this.m3 = "motor100";
+            this.m4 = "motor100";
             this.video1 = "lineSensor";
             this.video2 = "photo";
             this.getPhotoPort = "0";
